8ffb5c8ed98703c03d0c05444728ff4763bf363c
thom6a27 <thom6a27@iba.dk>
2024-02-15 10:27:24 +0100

    feature-4.3

--- hello_app/views.py
+++ hello_app/views.py
@@ -10,6 +10,10 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/create/")
+def about():
+    return render_template("createnote.html")
+
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
