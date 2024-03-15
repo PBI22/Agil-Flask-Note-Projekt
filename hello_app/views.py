8e3e0e522c7a72afc15b96280021b3e6cd48831f
MartinDP333 <Martindauer448@gmail.com>
2024-03-15 12:57:01 +0100

    added noteJSON

--- hello_app/views.py
+++ hello_app/views.py
@@ -30,6 +30,38 @@ def display_data():
     cursor.execute('''SELECT * FROM note''')
 
     rows = cursor.fetchall()
+
+
+
+    # Convert the data to a list of dictionaries
+    data_list = []
+    for row in rows:
+        # Assuming each row is represented as a dictionary with column names as keys
+        created_datetime_iso = datetime.strptime(row[3], '%Y-%m-%d %H:%M:%S.%f').isoformat()
+        lastedited_datetime_iso = datetime.strptime(row[4], '%Y-%m-%d %H:%M:%S.%f').isoformat()
+        data_dict = {
+            'noteID': row[0],
+            'title': row[1],
+            'text': row[2],
+            'created': created_datetime_iso,
+            'lastedited': lastedited_datetime_iso,
+            'imagelink': row[5],
+            'author': row[6],
+        }
+        #print(type(data_dict['created']))
+        data_list.append(data_dict)
+
+    # Close the database connection
+    conn.close()
+
+    # Convert the list of dictionaries to JSON
+    json_data = json.dumps(data_list, indent=4)
+
+    # Return JSON data
+    return jsonify(data_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
     
 
 # Create note
