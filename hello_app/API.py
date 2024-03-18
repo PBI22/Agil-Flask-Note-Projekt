1a0e19e51a287ebdc7c5ae3b1d11d054e742ed43
MartinDP333 <Martindauer448@gmail.com>
2024-03-18 13:16:05 +0100

    har flyttet api til at bruge ORM og har fjernet unødvendige imports, bruger dbsession i stedet for sqlite3 til at forbinde til databasen og hermed Note model til at hente data

--- hello_app/API.py
+++ hello_app/API.py
@@ -1,57 +1,30 @@
-import markdown2
-import json
-import os
 from datetime import datetime
-from flask import Blueprint, Flask, render_template, request, redirect, url_for, flash, session, jsonify
-from . import app
-from .models import Note, Account
+from flask import Blueprint, jsonify
 from .utils import *
-from json import JSONEncoder
-import sqlite3
-import requests
+from .models import Note
 
 api = Blueprint('api', __name__)
 
-@api.route('/noteJSON')
-def display_data():
-    # Connect to the SQLite database
-    conn = sqlite3.connect('database/db.sqlite')
-    cursor = conn.cursor()
-
-    # Execute a query to fetch data from the database
-    cursor.execute('''SELECT * FROM note''')
-
-    rows = cursor.fetchall()
-
-
-
-    # Convert the data to a list of dictionaries
+@api.route('/note', methods=['GET'])
+def display_notes():
+    all_notes = dbsession.query(Note).all()
     data_list = []
-    for row in rows:
-        # Assuming each row is represented as a dictionary with column names as keys
-        created_datetime_iso = datetime.strptime(row[3], '%Y-%m-%d %H:%M:%S.%f').isoformat()
-        lastedited_datetime_iso = datetime.strptime(row[4], '%Y-%m-%d %H:%M:%S.%f').isoformat()
+    for note in all_notes:
         data_dict = {
-            'noteID': row[0],
-            'title': row[1],
-            'text': row[2],
-            'created': created_datetime_iso,
-            'lastedited': lastedited_datetime_iso,
-            'imagelink': row[5],
-            'author': row[6],
+            'noteID': note.noteID,
+            'title': note.title,
+            'text': note.text,
+            'created': note.created.isoformat(),
+            'lastedited': note.lastedited.isoformat(),
+            'imagelink': note.imagelink,
+            'author': note.author,
         }
-        #print(type(data_dict['created']))
         data_list.append(data_dict)
 
-    # Close the database connection
-    conn.close()
-
-    # Convert the list of dictionaries to JSON
-    json_data = json.dumps(data_list, indent=4)
-
-    # Return JSON data
     return jsonify(data_list)
 
+def display_note(id):
+    pass
 
 
 """
