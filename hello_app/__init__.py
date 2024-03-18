4dcea964de15a89203d55eb88d90053057224534
MartinDP333 <Martindauer448@gmail.com>
2024-03-18 12:24:37 +0100

    løst lokal conflict

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -1,3 +1,9 @@
-from flask import Flask  # Import the Flask class
+from flask import Flask
+from datetime import timedelta
 app = Flask(__name__)    # Create an instance of the class for our use
 app.secret_key = 'secret_key' 
+
+# session cookie settings 
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=10) 
--- hello_app/views.py
+++ hello_app/views.py
@@ -6,13 +6,9 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from . import app
 from .models import Note, Account
 from .utils import *
-from json import JSONEncoder
-import sqlite3
-import requests
+from .auth import auth, login_required
 
-
-
-
+app.register_blueprint(auth, url_prefix='/auth')
 
 # Vores landing Page - Der viser listen over noter
 @app.route("/")
@@ -20,8 +16,8 @@ def home():
     return render_template("home.html", notes = updateList(), datetime = datetime.now())
 
 # Create note
-#Login required (what if you're already logged in)
 @app.route("/create/", methods=["GET","POST"])
+@login_required
 def create_note():
 
     if request.method == "GET":
@@ -33,8 +29,8 @@ def create_note():
 
     return redirect(url_for('home'))
 
-#Login required (what if you're already logged in =session)
 @app.route("/edit/<id>", methods=["GET","POST"])
+@login_required
 def edit(id = None):
 
     note = find_note(id)
@@ -68,6 +64,7 @@ def view(id = None):
 
 
 @app.route("/delete/<id>")
+@login_required
 def delete_note(id = None):
     
     if id is None:
@@ -97,67 +94,3 @@ def search_results():
     # Søg efter noter, der matcher søgeordet
     search_results = searchbar(query)
     return render_template('search_results.html', results=search_results, query=query)
-
-@app.route("/signup/", methods=["GET","POST"])
-def create_account():
-    if request.method == "POST":
-        # Create account post logik
-  
-        if not request.form['username']:
-            flash('Username is required', 'error')
-            return redirect(url_for('create_account'))
-        if not request.form['password']:
-            flash('Password is required', 'error')
-            return redirect(url_for('create_account'))
-        if not request.form['email']:
-            flash('Email is required', 'error')
-            return redirect(url_for('create_account'))
-        
-        # Check if username already exists
-        if dbsession.query(Account).filter_by(username=request.form['username']).first() is not None:
-            flash('Username already exists', 'error')
-            return redirect(url_for('create_account'))
-        
-        try:
-            dbsession.add(Account(username = request.form['username'], password = request.form['password'], email = request.form['email']))
-            dbsession.commit()
-            flash('Account created successfully!', 'success')
-            
-        except:
-            flash('Error creating account', 'error')
-            return redirect(url_for('create_account'))
-        return redirect(url_for('home'))
-        
-    else:
-        return render_template("signup.html")
-
-#login route
-@app.route('/login', methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        # Query the database for the account with provided username and password
-        account = dbsession.query(Account).filter_by(username=username, password=password).first()
-
-         
-        if account:   
-
-            session['user'] = account.username
-
-            flash(f'Login successful for {account.username}', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Invalid username or password', 'error')
-            return redirect(url_for('login'))
-
-    # If request method is GET, render the login template
-    return render_template('login.html')
-@app.route('/logout')
-def logout():
-    user_logout = session['user']
-    session.pop('user', None) 
-    flash(f'You have been logged out, {user_logout}', 'success')
-    return redirect(url_for('home'))
