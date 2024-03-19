8b597fa94dab11e5200619513cf0abcf6a483f7c
zteffano <48279765+zteffano@users.noreply.github.com>
2024-03-19 09:55:49 +0100

    implementering af error/critical logs i endpoints/funktioner

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from flask import render_template, send_from_directory
+from flask import render_template, send_from_directory, request
 from . import app
 from .utils import *
 from .log_config import setup_app_logging
@@ -16,11 +16,16 @@ setup_app_logging(app)
 
 @app.route("/")
 def home():
-    app.logger.info("Home page requested")
+    app.logger.info("Home page requested from: %s", request.remote_addr)
     return render_template("home.html", notes = updateList(), datetime = datetime.now())
 
 @app.route('/robots.txt')
 def robots_txt():
-    root_dir = os.path.abspath(os.path.dirname(__file__))
-    folder_path = os.path.join(root_dir, 'static')
-    return send_from_directory(app.static_folder, 'robots.txt')
+    try:
+        root_dir = os.path.abspath(os.path.dirname(__file__))
+        folder_path = os.path.join(root_dir, 'static')
+        app.logger.info("robots.txt requested from: %s", request.remote_addr)
+        return send_from_directory(app.static_folder, 'robots.txt')
+    except Exception as e:
+        app.logger.error(f"Failed to serve robots.txt: {e}")
+        raise
