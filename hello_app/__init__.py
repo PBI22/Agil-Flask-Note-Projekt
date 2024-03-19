ffc1d5d92630cbd611c0fc4e0002003575de49f0
Marcus <marcusvang123@gmail.com>
2024-03-19 11:52:17 +0100

    createnote tilkobling

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -2,15 +2,8 @@ import os
 from flask import Flask
 from datetime import timedelta
 
-<<<<<<< HEAD
 app = Flask(__name__)    # Create an instance of the class for our use
 app.secret_key = 'secret_key' 
-=======
-#app = Flask(__name__)    # Create an instance of the class for our use
-#app.secret_key = 'secret_key' 
-app = Flask(__name__, instance_relative_config=True)
-app.secret_key = os.get.environ('SECRET_KEY')
->>>>>>> b4f2c461131d9ea4c6917e1921e3e676690c1d00
 
 # session cookie settings 
 app.config['SESSION_COOKIE_HTTPONLY'] = True
