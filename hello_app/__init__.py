2d7e17b960cf5d5b1a3233e56d5c3ee89aed9b15
thom6a27 <118893723+thom6a27@users.noreply.github.com>
2024-03-19 11:53:17 +0100

    Update __init__.py

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 #app = Flask(__name__)    # Create an instance of the class for our use
 #app.secret_key = 'secret_key' 
 app = Flask(__name__, instance_relative_config=True)
-app.secret_key = os.get.environ('SECRET_KEY')
+app.secret_key = os.environ.get('SECRET_KEY')
 
 # session cookie settings 
 app.config['SESSION_COOKIE_HTTPONLY'] = True
