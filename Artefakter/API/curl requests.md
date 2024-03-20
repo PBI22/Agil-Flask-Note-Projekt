9f420ff5912b75c3d7afd1915795454c93af6f70
MartinDP333 <Martindauer448@gmail.com>
2024-03-20 09:37:31 +0100

    Tilføjede edit/<id> endpoint og tilføjede try except til hver api endpoint

--- Artefakter/API/curl requests.md
+++ Artefakter/API/curl requests.md
@@ -1,11 +1,22 @@
-## Curl
-Curl kan bruges i cmd til at lave en request til en API som hermed vil lave en GET eller POST request.
-De vil blive dokumenteret her for at kunne fremhæve test af API og at det virker
+## cURL
+cURL kan bruges i cmd til at lave en request til en API som hermed vil lave en GET eller POST request.
+De vil blive dokumenteret her for at kunne fremhæve test af API og at det virker.
+cURL bruge som en løsning til test af implementation af API. Der kan være muligheder for at indføre en anden løsning som muligvis postman eller swagger. Swagger kan eksempelvis implementeres med flasgger library.
 
-# Curl request til create
-requesten laver en ny note som giver en titel, text og image link.
+# cURL request til create
+Requesten laver en ny note som giver en titel, text og image link.
 created og lastedited bruger lokalt datetime.now() og er dermed ikke eftersøgt
 Author bliver givet id 1 (er ikke helt sikker på hvordan man vil tillade/verificere hvem authoren er når det er igennem API'en)
 ```
-curl -X POST -H "Content-Type: application/json" -d "{\"title\":\"Title posted through API\",\"text\":\"Text posted through API\",\"imagelink\":\"Image posted through API\"}" http://localhost:5000/api/create
+curl -X POST -H "Content-Type: application/json" -d "{\"title\":\"Title posted through API\",\"text\":\"Text posted through API\",\"imagelink\":\"Image posted through API\",\"author\": 1 }" http://localhost:5000/api/create
 ```
+
+# cURL request til edit
+Requesten tager noten med id givet i URL parameter. Hermed vil man kunne indsætte title, text og imagelink. Hvis noget ikke bliver indsat, så ændre den det ikke. Så hvis text er tom, men title og imagelink har noget skrevet, så vil title og imagelink ændres og text forblive det samme. Lastedited vil også automatisk blive ændret til det nye tidspunkt. Eksempel på en edit kan ses under med cURL som ændre title, text og imagelink ved id 12.
+```
+curl -X PUT -H "Content-Type: application/json" -d "{\"title\": \"Updated Title\", \"text\": \"Updated Text\", \"imagelink\": \"Updated Image Link\"}" http://localhost:5000/api/edit/12
+```
+
+# cURL til delete og view
+Bruger ikke noget input andet end id i URL så cURL behøves ikke
+Skal dog noteres at alle kan bruge delete medmindre JWT anvendes
--- hello_app/API.py
+++ hello_app/API.py
@@ -2,41 +2,52 @@ from datetime import datetime
 from flask import Blueprint, jsonify, request
 from .utils import *
 from .models import Note
+from sqlalchemy.exc import SQLAlchemyError
 
 api = Blueprint('api', __name__)
 
 # Tager alle noter
 @api.route('/view', methods=['GET'])
 def display_notes():
-    all_notes = dbsession.query(Note).all()
-    note_isoformat = [note.to_isoformat() for note in all_notes]
-    return jsonify(note_isoformat)
+    try:
+        all_notes = dbsession.query(Note).all()
+        note_isoformat = [note.to_isoformat() for note in all_notes]
+        return jsonify(note_isoformat)
+    except SQLAlchemyError as e:
+        return jsonify({'Error': str(e)}), 500
+
 
 # Tager en note fra id
 @api.route('/view/<id>', methods=['GET'])
 def display_note(id):
-    print("ID: " + id)
-    note_from_id = dbsession.query(Note).filter(Note.noteID == id).all()
-    note_isoformat = [note.to_isoformat() for note in note_from_id]
-    return jsonify(note_isoformat)
-
+    try:
+        note_from_id = dbsession.query(Note).filter(Note.noteID == id).all()
+        note_isoformat = [note.to_isoformat() for note in note_from_id] # Tager alle med specifikt id, det virker det dårligt men gider ikke
+        return jsonify(note_isoformat)
+    except SQLAlchemyError as e:
+        return jsonify({'Error': str(e)}), 500
 # Tager title, text og imagelink til at lave en post request(curl kan eksempelvis bruges. Check "curl requests.md" i Artefakter/API)
 @api.route('/create', methods=['POST'])
 def create_note():
-    note = request.json
-    title = note.get('title')
-    text = note.get('text')
-    created = datetime.now()
-    lastedited = datetime.now()
-    imagelink = note.get('imagelink')
-    author = 1
+    try:
+        note = request.json
+        title = note.get('title')
+        text = note.get('text')
+        created = datetime.now()
+        lastedited = datetime.now()
+        imagelink = note.get('imagelink')
+        author = note.get('author')
 
-    new_note = Note(title = title, text = text, created = created, lastedited = lastedited, imagelink = imagelink, author = author)
+        new_note = Note(title = title, text = text, created = created, lastedited = lastedited, imagelink = imagelink, author = author)
 
-    dbsession.add(new_note)
-    dbsession.commit()
-    return jsonify({'message': 'Note created successfully', 'noteID': new_note.noteID}), 201
+        dbsession.add(new_note)
+        dbsession.commit() 
+        return jsonify({'message': 'Note created successfully', 'noteID': new_note.noteID}), 201
+    except SQLAlchemyError as e:
+        dbsession.rollback()
+        return jsonify({'Error': str(e)}), 500
 
+# Sletter en note ud fra id (muligvis skal der senere tilføjes sikkerhed, så ikke alle kan slette noter ud fra APIen)
 @api.route('/delete/<id>')
 def delete_note(id):
     try:
@@ -47,8 +58,40 @@ def delete_note(id):
             return "Note with id: " + id + " has been deleted"
         else:
             return "Error deleting note", 404
-    except Exception as e:
+    except SQLAlchemyError as e:
          dbsession.rollback()
          return str(e), 500
     except:
             return "Error deleting note", 404
+
+# Redigere en eksisterende note ud fra id og input(Kan testes med cURL request i "curl requests.md" fundet i mappen Artefakter/API)
+@api.route('/edit/<id>', methods=['PUT'])
+def edit_note(id):
+    try:
+        edit_query = dbsession.query(Note).filter(Note.noteID == id).first()
+
+        if not edit_query:
+            return jsonify({"message": "Note not found"}), 404
+        
+        note = request.json
+        title = note.get('title')
+        text = note.get('text')
+        lastedited = datetime.now()
+        imagelink = note.get('imagelink')
+
+        # Laves så kun ændrede data tabeller bliver ændret(ellers gør den det andet tomt)
+        if title is not None:
+            edit_query.title = title
+        if text is not None:
+            edit_query.text = text
+        if imagelink is not None:
+            edit_query.imagelink = imagelink
+        edit_query.lastedited = lastedited
+
+        dbsession.commit()
+
+        return jsonify({"message": "Note updated successfully"}), 200
+    except SQLAlchemyError as e:
+        dbsession.rollback()
+        return jsonify({'Error': str(e)}), 500
+
