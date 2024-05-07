88111b14a6b6a882e07db85263fd9ab7f7854a75
MartinDP333 <Martindauer448@gmail.com>
2024-05-07 12:53:35 +0200

    Quick fix af mange issues i api.py, auth.py og dbconnect.py

--- hello_app/auth.py
+++ hello_app/auth.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash, session, Response
+from functools import wraps
+from flask import (
+    Blueprint,
+    render_template,
+    request, redirect,
+    url_for,
+    flash,
+    session
+    )
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import Account
 from .utils import dbsession
-from functools import wraps
 from . import app
 
 
@@ -10,6 +17,26 @@ from . import app
 auth = Blueprint('auth', __name__)
 
 def login_required(f):
+    """
+    Decorator function that checks if a user is logged in before allowing access to a view function.
+
+    Parameters:
+        f (function): The view function to be decorated.
+
+    Returns:
+        function: The decorated view function.
+
+    Notes:
+        This decorator function checks if the 'user' key is present in the session.
+        If not, it flashes an error message and redirects the user to the login page.
+        If the 'user' key is present, it calls the original view function.
+
+    Example:
+        @login_required
+        def my_view_function():
+            # Code for the view function
+            pass
+    """
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'user' not in session:
@@ -20,12 +47,27 @@ def login_required(f):
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    """
+    This function handles the login functionality for the application.
+
+    Parameters:
+    - None
+
+    Returns:
+    - If the request method is POST and the login is successful, it redirects the user to the home page.
+    - If the request method is POST and the login fails, it redirects the user back to the login page.
+    - If the request method is GET, it renders the login template.
+
+    Raises:
+    - None
+
+    """
     if request.method == "POST":
         username = request.form.get('username')
         password = request.form.get('password')
 
         account = dbsession.query(Account).filter_by(username=username).first()
-        if account and check_password_hash(account.password, password):  
+        if account and check_password_hash(account.password, password):
             session.clear()
             #gem bruger id og  role i session.
             session['user'] = account.username
@@ -35,27 +77,60 @@ def login():
 
             flash(f'Login successful for {account.username}', 'success')
             return redirect(url_for('home'))
-        else:
-            flash('Invalid username or password', 'error')
-            app.logger.warning(f"Failed login attempt from: {request.remote_addr} with username: {username}")
-            return redirect(url_for('auth.login'))
+        flash('Invalid username or password', 'error')
+        app.logger.warning(f"Failed login attempt from: {request.remote_addr} with username: {username}")
+        return redirect(url_for('auth.login'))
 
     # If request method is GET, render the login template
     return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
+    """
+    Logout the current user.
+
+    This function clears the session and redirects the user to the home page.
+    It also displays a flash message to notify the user that they have been logged out.
+
+    Returns:
+        redirect: A redirect response to the home page.
+
+    """
     user_logout = session['user']
-    session.clear() 
+    session.clear()
     flash(f'You have been logged out, {user_logout}', 'success')
     return redirect(url_for('home'))
 
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def create_account():
+    """
+    Create a new user account.
+
+    This function handles the creation of a new user account.
+    It is accessed through the '/signup' route and supports both GET and POST methods.
+
+    Parameters:
+        None
+
+    Returns:
+        If the request method is GET, it renders the 'signup.html' template.
+        If the request method is POST:
+            - If the required fields (username, password, email) are not provided,
+              it redirects to the 'create_account' route and displays appropriate flash messages.
+            - If the username already exists,
+              it redirects to the 'auth.create_account' route and displays a flash message.
+            - If the account creation is successful,
+              it redirects to the 'home' route and displays a success flash message.
+            - If an exception occurs during the account creation process,
+              it redirects to the 'auth.create_account' route and displays an error flash message.
+
+    Raises:
+        None
+    """
     if request.method == "POST":
         # Create account post logik
-  
+
         if not request.form['username']:
             flash('Username is required', 'error')
             return redirect(url_for('create_account'))
@@ -65,20 +140,27 @@ def create_account():
         if not request.form['email']:
             flash('Email is required', 'error')
             return redirect(url_for('create_account'))
-        
+
         # Check if username already exists
         if dbsession.query(Account).filter_by(username=request.form['username']).first() is not None:
             flash('Username already exists', 'error')
             return redirect(url_for('auth.create_account'))
-        
+
         try:
             username = request.form['username']
             password = request.form['password']
             email = request.form['email']
-            roleID = 1
+            role_id = 1
             hashed_password = generate_password_hash(password, method='pbkdf2', salt_length=16)
-            
-            dbsession.add(Account(username = username, password = hashed_password, email = email, roleID = roleID))
+
+            dbsession.add(
+                Account(
+                    username=username,
+                    password=hashed_password,
+                    email=email,
+                    roleID=role_id,
+                )
+            )
             dbsession.commit()
             flash('Account created successfully!', 'success')
             # Logger automatisk brugeren ind efter oprettelse
@@ -88,12 +170,11 @@ def create_account():
             session['userID'] = account.accountID
             session['userEmail'] = account.email
             return redirect(url_for('home'))
-            
+
         except Exception as e:
             app.logger.error(f"Failed to create account: {e} from address: {request.remote_addr} with username: {username} and email: {email}")
             flash('Error creating account', 'error')
             return redirect(url_for('auth.create_account'))
-        
-        
+
     else:
         return render_template("signup.html")
--- hello_app/dbconnect.py
+++ hello_app/dbconnect.py
@@ -1,16 +1,39 @@
 import os
-from . import app
-from sqlalchemy import create_engine, inspect
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+from . import app
 from .models import Base
 TESTING = os.environ.get("TESTING", False)
 # Construct an absolute path to where the database file should be located.
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(BASE_DIR, "database", "db.sqlite")
 
-def get_engine(Testing = False):
+def get_engine(testing = False):
+    """
+    Returns a SQLAlchemy engine object based on the specified configuration.
 
-    if Testing:
+    Parameters:
+        testing (bool): If True, a test database engine will be created. 
+        If False, a regular database engine will be created. Default is False.
+
+    Returns:
+        engine: A SQLAlchemy engine object.
+
+    Raises:
+        Exception: If there is an error connecting to the database.
+
+    Notes:
+        - If testing is True, the function will create a test database engine 
+          and delete any existing test database file to ensure a fresh structure.
+        - If testing is False, the function will create a regular database engine
+          using the specified database path.
+        - The database path is determined by the BASE_DIR and db_path variables defined in the module.
+        - The engine object is created using the create_engine function from the sqlalchemy module.
+        - If an error occurs while connecting to the database, 
+          an exception will be raised and logged using the app.logger object.
+
+    """
+    if testing:
         # Ændrer stien til en specifik test database
         test_db_path = os.path.join(BASE_DIR,"database", "test.db")
         # Sletter test.db filen hvis den eksisterer, for at sikre den bygges på ny og ikke bruger gammel struktur
