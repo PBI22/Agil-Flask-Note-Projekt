3ba6a5e06f2f0c17e5723eb1de8505d5d71e6c91
zteffano <48279765+zteffano@users.noreply.github.com>
2024-05-09 15:37:22 +0200

    opsæt af CSRF protection af app igennem Flask WTF

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -7,12 +7,18 @@ setting the SameSite attribute for cookies, and defining a session lifetime. """
 import os
 from datetime import timedelta
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 
 app = Flask(__name__)  # Create an instance of the class for our use
+
+# Set secret_key before adding CSRF protection
 app.secret_key = os.environ.get("SECRET_KEY", "secret_key")
 
-# session cookie settings
+# Initialize CSRF protection
+csrf = CSRFProtect(app) 
+
+# Session cookie settings
 app.config["SESSION_COOKIE_HTTPONLY"] = True
 app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
 app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=10)
