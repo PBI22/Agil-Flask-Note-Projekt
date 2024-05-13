01187f22bea71326dc93331c29002adbab88f0cb
zteffano <48279765+zteffano@users.noreply.github.com>
2024-05-13 12:57:26 +0200

    små issues fixes i de filer de ikke når threshold scoren på 8 og derfor fejler. Så vi nu kan kører med den threshold som standard.

--- hello_app/models/FlaskForms.py
+++ hello_app/models/FlaskForms.py
@@ -1,18 +1,43 @@
+# pylint: disable=C0103
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, EmailField, TextAreaField, HiddenField
+from wtforms import (
+    StringField,
+    PasswordField,
+    SubmitField,
+    EmailField,
+    TextAreaField,
+    HiddenField,
+)
 from wtforms.validators import DataRequired, Email, EqualTo
 
 
 class LoginForm(FlaskForm):
     """
     Form for handling user login with username and password fields.
+
+    Attributes:
+        username (StringField): Field for entering the username.
+        password (PasswordField): Field for entering the password.
+        submit (SubmitField): Button for submitting the login form.
     """
+
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
 
 
 class SignUpForm(FlaskForm):
+    """
+    Form for handling user sign up with username, email, password, and confirm password fields.
+
+    Attributes:
+        username (StringField): Field for entering the username.
+        email (EmailField): Field for entering the email.
+        password (PasswordField): Field for entering the password.
+        confirm_password (PasswordField): Field for confirming the password.
+        submit (SubmitField): Button for submitting the sign up form.
+    """
+
     username = StringField("Username", validators=[DataRequired()])
     email = EmailField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
@@ -23,6 +48,16 @@ class SignUpForm(FlaskForm):
 
 
 class NoteForm(FlaskForm):
+    """
+    Form for creating a new note with title, text, and optional image link fields.
+
+    Attributes:
+        title (StringField): Field for entering the title of the note.
+        text (TextAreaField): Field for entering the text content of the note.
+        imagelink (HiddenField): Field for storing the optional image link of the note.
+        submit (SubmitField): Button for submitting the note form.
+    """
+
     title = StringField("Title", validators=[DataRequired()])
     text = TextAreaField("Note", validators=[DataRequired()])
     imagelink = HiddenField()  # Handling this as hidden for now; modify as needed
@@ -30,6 +65,16 @@ class NoteForm(FlaskForm):
 
 
 class EditNoteForm(FlaskForm):
+    """
+    Form for editing an existing note with title, text, and optional image link fields.
+
+    Attributes:
+        title (StringField): Field for entering the updated title of the note.
+        text (TextAreaField): Field for entering the updated text content of the note.
+        imagelink (HiddenField): Field for storing the updated optional image link of the note.
+        submit (SubmitField): Button for submitting the edited note form.
+    """
+
     title = StringField("Title", validators=[DataRequired()])
     text = TextAreaField("Note", validators=[DataRequired()])
     imagelink = HiddenField()
